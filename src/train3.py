a91e4baea61fb9d7a98061d5ed6f6ad1f5ac6985
Panke_PC <18230063520@163.com>
2025-08-19 22:04:56 +0800

    2025/8/19实验

--- src/train3.py
+++ src/train3.py
@@ -78,8 +78,8 @@ def train():
     # ========================================================================================
     # ==================================注意修改此值============================================
     # ========================================================================================
-    model = models.UNetHybridAttentionV23_2_Ablation1().to(device)
-    model_description = '基于UNetHybridAttentionV23_2,仅保留频域分支'
+    model = models.UNetHybridAttentionV23_2().to(device)
+    model_description = '基于UNetHybridAttentionV23，将HybridAttention前边加入卷积；'
     model_name = model.model_name
     # model_name = 'UNetHybridAttentionV23_2'
     expt_id = generate_experiment_id(model=model_name,
